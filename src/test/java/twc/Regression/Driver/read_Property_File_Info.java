80a58b8e03fd0bb33f7ff1065a4b07291dcab3ef
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2020-11-18 16:31:46 +0530

    Update read_Property_File_Info.java

--- src/test/java/twc/Regression/Driver/read_Property_File_Info.java
+++ src/test/java/twc/Regression/Driver/read_Property_File_Info.java
@@ -14,7 +14,7 @@ public class read_Property_File_Info {
 	public static void property() throws IOException
 	
 	{
-		String dataFilePath="/Users/jenkins/Documents/projects/AndroidPrivacy/DataFile.Properties";
+		String dataFilePath="/Users/aswinikumar/Documents/adsandroid-automation-master/AndroidPrivacy/DataFile.Properties";
 		
 		File file = new File(dataFilePath);
 		try {
