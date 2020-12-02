f2b4ba971c98d78836cf82ae97f4333286ba3929
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2020-12-02 13:41:04 +0530

    Update EXEMPT_Run.java

--- src/test/java/twc/Regression/TestCases/EXEMPT_Run.java
+++ src/test/java/twc/Regression/TestCases/EXEMPT_Run.java
@@ -21,8 +21,8 @@ public class EXEMPT_Run extends TwcAndroidBaseTest {
 	
 	@BeforeClass(alwaysRun = true)
 	public void beforeClass() {
-		System.out.println("****** LATAM EXEMPT Privacy Test Started");
-		logStep("****** LATAM EXEMPT  Privacy Test Started");
+		System.out.println("****** EXEMPT Privacy Test Started");
+		logStep("****** EXEMPT  Privacy Test Started");
 		this.configFile = this.rewriteRuleToEnableEXEMPT(CONFIG_FILE_PATH);
 		this.proxy = new CharlesProxy("localhost", 8222, CONFIG_FILE_PATH);
 
@@ -41,12 +41,12 @@ public class EXEMPT_Run extends TwcAndroidBaseTest {
 		this.proxy.disableRewriting();
 		//this.proxy.quitCharlesProxy();
 		
-		System.out.println("****** LATAM EXEMPT   Privacy Test Ended");
-		logStep("****** LATAM EXEMPT  Privacy Test Ended");
+		System.out.println("****** EXEMPT   Privacy Test Ended");
+		logStep("****** EXEMPT  Privacy Test Ended");
 	}
 	
 	@Test(priority = 1)
-	public void preConditionsTest_for_LATAMEXEMPT() throws Exception {
+	public void preConditionsTest_for_EXEMPT() throws Exception {
 		// Enable rewriting on Charles install/launch TWC
 		this.proxy.enableRewriting();
 		this.proxy.startRecording();
@@ -68,141 +68,141 @@ public class EXEMPT_Run extends TwcAndroidBaseTest {
 		AppiumFunctions.ClickonIUnderstand();
 	//	Functions.close_launchApp();
 		//Utils.navigateToAllCards(false);
-		//CharlesFunctions.archive_folder("charles");
+		CharlesFunctions.archive_folder("charles");
 		this.proxy.getXml();
 	//	Utils.createXMLFileForCharlesSessionFile();
 	}
 	@Test(priority =2,enabled = true)  
-	 @Title("Verifying Lotame ad.crwdcntrl.net api call supressing for LATAM_EXEMPT  privacy") 
-	public void Verifying_Loatme_adcrwdcntrlnet_apiCall_supressing_Latam_EXEMPT_Privacy() throws Exception {	  
-	 System.out. println("=================Verifying Lotame ad.crwdcntrl.net api call supressing for LATAM_EXEMPT  privacy testcase started =========================" ); 
+	 @Title("Verifying Lotame ad.crwdcntrl.net api call supressing for EXEMPT  privacy") 
+	public void Verifying_Loatme_adcrwdcntrlnet_apiCall_supressing_EXEMPT_Privacy() throws Exception {	  
+	 System.out. println("=================Verifying Lotame ad.crwdcntrl.net api call supressing for EXEMPT  privacy testcase started =========================" ); 
 	 Functions.validating_adcrw_privacy_Optoutmode_scenarion(); 
-	  System.out. println("================= Verifying Lotame ad.crwdcntrl.net api call supressing for LATAM_EXEMPT  privacy  testcase End =========================" );
+	  System.out. println("================= Verifying Lotame ad.crwdcntrl.net api call supressing for EXEMPT  privacy  testcase End =========================" );
 	  }
 	
 
 	@Test(priority =4,enabled = true)  
-	 @Title("Verifying Lotame bcp.crwdcntrl.net api call supressing for LATAM_EXEMPT  privacy") 
-	public void Verifying_Loatme_bcpcrwdcntrlnet_apiCall_supressing_Latam_EXEMPT_Privacy() throws Exception {	  
-	 System.out. println("=================Verifying Lotame bcp.crwdcntrl.net api call supressing for LATAM_EXEMPT  privacy testcase started =========================" ); 
+	 @Title("Verifying Lotame bcp.crwdcntrl.net api call supressing for EXEMPT  privacy") 
+	public void Verifying_Loatme_bcpcrwdcntrlnet_apiCall_supressing_EXEMPT_Privacy() throws Exception {	  
+	 System.out. println("=================Verifying Lotame bcp.crwdcntrl.net api call supressing for EXEMPT  privacy testcase started =========================" ); 
 	 Functions.validating_bcp_privacy_Optoutmode_scenarion();
-	  System.out. println("=================Verifying Lotame bcp.crwdcntrl.net api call supressing for LATAM_EXEMPT  privacy testcase End =========================" );
+	  System.out. println("=================Verifying Lotame bcp.crwdcntrl.net api call supressing for EXEMPT  privacy testcase End =========================" );
 	  }
 	
 	@Test(priority =6,enabled = true)  
-	 @Title("Verifying Factual location.wfxtriggers.com api call supressing for LATAM_EXEMPT  privacy")  
-	public void Verifying_Factual_locationwfxtriggerscom_apiCall_supressing_Latam_EXEMPT_Privacy() throws Exception {	  
-	 System.out. println("=================Verifying Factual location.wfxtriggers.com api call supressing for LATAM_EXEMPT  privacy started =========================" ); 
+	 @Title("Verifying Factual location.wfxtriggers.com api call supressing for EXEMPT  privacy")  
+	public void Verifying_Factual_locationwfxtriggerscom_apiCall_supressing_EXEMPT_Privacy() throws Exception {	  
+	 System.out. println("=================Verifying Factual location.wfxtriggers.com api call supressing for EXEMPT  privacy started =========================" ); 
 	 Functions.validating_Fatualcall_privacy_Optoutmode_scenarion();
-	  System.out. println("================= Verifying Factual location.wfxtriggers.com api call supressing for LATAM_EXEMPT  privacy End =========================" );
+	  System.out. println("================= Verifying Factual location.wfxtriggers.com api call supressing for EXEMPT  privacy End =========================" );
 	  }
 	
 
 	 @Test(priority=8,enabled = true)  
-	  @Title("Verifying supress amazon slot id for  home screen hourly preload ad call LATAM_EXEMPT Privacy") 
-	  public void Verifying_Supress_amazon_Slotid_homescreenhourly_preload_adcall_Latam_EXEMPT_Privacy()throws Exception { 
-	  System.out.println("=================Verifying supress amazon slot id for  home screen hourly preload ad call LATAM_EXEMPT Privacy test case Started========================="); 
+	  @Title("Verifying supress amazon slot id for  home screen hourly preload ad call EXEMPT Privacy") 
+	  public void Verifying_Supress_amazon_Slotid_homescreenhourly_preload_adcall_EXEMPT_Privacy()throws Exception { 
+	  System.out.println("=================Verifying supress amazon slot id for  home screen hourly preload ad call EXEMPT Privacy test case Started========================="); 
 	//  Functions.get_aaxcal_homescreen_hourly(); 
-	  logStep("Verifying supress amazon slot id for  home screen hourly preload ad call LATAM_EXEMPT Privacy");
+	  logStep("Verifying supress amazon slot id for  home screen hourly preload ad call EXEMPT Privacy");
 	  Functions.verifyaax_SlotId_supress("869c843c-7cf8-47ae-b6ed-088057e4bc8a");
-	  System.out.println("=================Verifying supress amazon slot id in home screen hourly preload ad call for LATAM_EXEMPT Privacy  test case  End ========================="); 
+	  System.out.println("=================Verifying supress amazon slot id in home screen hourly preload ad call for EXEMPT Privacy  test case  End ========================="); 
 	  }
 	
 	 
 		@Test(priority = 10, enabled = true)
-		@Title("Verifying supress amazon Slot Id for  feed1 preload ad call LATAM_EXEMPT Privacy")
-		public void Verifying_supress_amazon_Slotid_feed1_preroladcall_Latam_EXEMPT_Privacy() throws Exception {
-			System.out.println("=================Verifying supress amazon Slot Id for  feed1 preload ad call LATAM_EXEMPT Privacy testcase Started =========================");
-			logStep("Verifying supress amazon Slot Id for  feed1 prerol ad call LATAM_EXEMPT Privacy");
+		@Title("Verifying supress amazon Slot Id for  feed1 preload ad call EXEMPT Privacy")
+		public void Verifying_supress_amazon_Slotid_feed1_preroladcall_EXEMPT_Privacy() throws Exception {
+			System.out.println("=================Verifying supress amazon Slot Id for  feed1 preload ad call EXEMPT Privacy testcase Started =========================");
+			logStep("Verifying supress amazon Slot Id for  feed1 prerol ad call EXEMPT Privacy");
 			//Functions.get_aaxcal_feed1();
 			  Functions.verifyaax_SlotId_supress("f4b66249-b6eb-4155-9d90-1e2b04487c99");
-			System.out.println("=================Verifying supress amazon Slot Id for  feed1 preload ad call LATAM_EXEMPT Privacy testcase End =========================");
+			System.out.println("=================Verifying supress amazon Slot Id for  feed1 preload ad call EXEMPT Privacy testcase End =========================");
 
 		}
 	
 	@Test(priority = 12, enabled = true)
-	@Title("Verifying supress amazon SlotId for feed2 prerload ad call  LATAM_EXEMPT Privacy")
-	public void Verifying_supress_amazon_Slotid_feed2_preroladcall_Latam_EXEMPT_Privacy() throws Exception {
-		System.out.println("=================Verifying supress amazon SlotId for feed2 prerload ad call  LATAM_EXEMPT Privacy testcase Started =========================");
-		logStep("Verifying supress amazon SlotId for feed2 prerload ad call  LATAM_EXEMPT Privacy");
+	@Title("Verifying supress amazon SlotId for feed2 prerload ad call EXEMPT Privacy")
+	public void Verifying_supress_amazon_Slotid_feed2_preroladcall_EXEMPT_Privacy() throws Exception {
+		System.out.println("=================Verifying supress amazon SlotId for feed2 prerload ad call EXEMPT Privacy testcase Started =========================");
+		logStep("Verifying supress amazon SlotId for feed2 prerload ad call EXEMPT Privacy");
 		//Functions.get_aaxcal_feed2();
 		  Functions.verifyaax_SlotId_supress("752a96eb-3198-4991-b572-17ec04883b6c");
-		System.out.println("=================Verifying supress amazon SlotId for feed2 prerload ad call  LATAM_EXEMPT Privacy  testcase End =========================");
+		System.out.println("=================Verifying supress amazon SlotId for feed2 prerload ad call EXEMPT Privacy  testcase End =========================");
 	}
 
 	 @Test(priority = 14, enabled = true)
-		@Title("Verifying supress amazon SlotId for  hourly details preload ad call LATAM_EXEMPT Privacy")
-		public void Verifying_Supress_amazon_Slotid_Hourlydetails_preload_adcall_Latam_EXEMPT_Privacy() throws Exception {
-		System.out.println("=================Verifying supress amazon Slot Id for  hourly details preload ad call LATAM_EXEMPT Privacy testcase  Started =========================");
-		logStep("Verifying supress amazon Slot Id for  hourly details preload ad call LATAM_EXEMPT Privacy");
+		@Title("Verifying supress amazon SlotId for  hourly details preload ad call EXEMPT Privacy")
+		public void Verifying_Supress_amazon_Slotid_Hourlydetails_preload_adcall_EXEMPT_Privacy() throws Exception {
+		System.out.println("=================Verifying supress amazon Slot Id for  hourly details preload ad call EXEMPT Privacy testcase  Started =========================");
+		logStep("Verifying supress amazon Slot Id for  hourly details preload ad call EXEMPT Privacy");
 		//Functions.get_aaxcal_Hourly();
 		  Functions.verifyaax_SlotId_supress("9be28769-4207-4d51-8063-dc8e645383b2");
-		System.out.println("================= Verifying supress amazon Slot Id for  hourly details preload ad call LATAM_EXEMPT Privacy testcase  End =========================");
+		System.out.println("================= Verifying supress amazon Slot Id for  hourly details preload ad call EXEMPT Privacy testcase  End =========================");
 		}
 		
 		  @Test(priority =16, enabled = true)
-			@Title("Verifying supress amazon SlotId for  hourly1 details big ad  preload call LATAM_EXEMPT Privacy")
-			public void Verifying_Supress_amazon_Slotid_Hourly1_bigaddetails_preload_adcall_Latam_EXEMPT_Privacy() throws Exception {
+			@Title("Verifying supress amazon SlotId for  hourly1 details big ad  preload call EXEMPT Privacy")
+			public void Verifying_Supress_amazon_Slotid_Hourly1_bigaddetails_preload_adcall_EXEMPT_Privacy() throws Exception {
 				System.out.println(
-						"=================Verifying supress amazon SlotId for  hourly1 details big ad  preload call LATAM_EXEMPT Privacy  test case Started =========================");
+						"=================Verifying supress amazon SlotId for  hourly1 details big ad  preload call EXEMPT Privacy  test case Started =========================");
 				//Functions.get_aaxcal_Hourly1();
 				  Functions.verifyaax_SlotId_supress("08f0ccea-cab5-449c-963d-dc57ed9ee87d");
-				System.out.println("=================Verifying supress amazon SlotId for  hourly1 details big ad  preload call LATAM_EXEMPT Privacy test case  End =========================");
+				System.out.println("=================Verifying supress amazon SlotId for  hourly1 details big ad  preload call EXEMPT Privacy test case  End =========================");
 
 			}
 			
 
 
 			@Test(priority = 18, enabled = true)
-			@Title("Verifying supress amazon SlotId for  hourly2 details big ad  preload call  LATAM_EXEMPT Privacy")
-			public void Verifying_supress_amazon_Slotid_Hourly2_bigaddetails_preload_adcall_Latam_EXEMPT_Privacy() throws Exception {
-				System.out.println("=================Verifying supress amazon SlotId for  hourly2 details big ad  preload call  LATAM_EXEMPT Privacy test case Started =========================");
+			@Title("Verifying supress amazon SlotId for  hourly2 details big ad  preload call EXEMPT Privacy")
+			public void Verifying_supress_amazon_Slotid_Hourly2_bigaddetails_preload_adcall_EXEMPT_Privacy() throws Exception {
+				System.out.println("=================Verifying supress amazon SlotId for  hourly2 details big ad  preload call EXEMPT Privacy test case Started =========================");
 				//Functions.get_aaxcal_Hourly2();
 				 Functions.verifyaax_SlotId_supress("4fbed16a-cc6f-4cb1-94f7-81465acbd47");
-				System.out.println("=================Verifying supress amazon SlotId for  hourly2 details big ad  preload call  LATAM_EXEMPT Privacy test case  End =========================");
+				System.out.println("=================Verifying supress amazon SlotId for  hourly2 details big ad  preload call EXEMPT Privacy test case  End =========================");
 
 			}
 
 
 
 			@Test(priority = 20, enabled = true)
-			@Title("Verifying supress amazon SlotId for  hourly3 details big ad preload call LATAM_EXEMPT Privacy")
-			public void Verifying_supress_amazon_Slotid_Hourly3_bigaddetails_preload_adcall_Latam_EXEMPT_Privacy()throws Exception {
-				System.out.println("=================Verifying supress amazon SlotId for  hourly3 details big ad preload call LATAM_EXEMPT Privacy test case  Started =========================");
+			@Title("Verifying supress amazon SlotId for  hourly3 details big ad preload call EXEMPT Privacy")
+			public void Verifying_supress_amazon_Slotid_Hourly3_bigaddetails_preload_adcall_EXEMPT_Privacy()throws Exception {
+				System.out.println("=================Verifying supress amazon SlotId for  hourly3 details big ad preload call EXEMPT Privacy test case  Started =========================");
 			//	Functions.get_aaxcal_Hourly3();
 				 Functions.verifyaax_SlotId_supress("2634dc9-b59f-4b2c-b281-bb3be291b7b6");
-				System.out.println("=================Verifying supress amazon SlotId for  hourly3 details big ad preload call LATAM_EXEMPT Privacy  test case End =========================");
+				System.out.println("=================Verifying supress amazon SlotId for  hourly3 details big ad preload call EXEMPT Privacy  test case End =========================");
 			}
 			  
 
 		
 		@Test(priority = 22, enabled = true)
-		@Title("Verifying supress amazon SlotId for maps details preload ad call LATAM_EXEMPT Privacy")
-		public void Verifying_supress_amazon_Slotid_mapsdetails_preload_adcall_Latam_EXEMPT_Privacy() throws Exception {
-			System.out.println("=================Verifying supress amazon SlotId for maps details preload ad call LATAM_EXEMPT Privacy  testcase  Started =========================");
-			logStep("Verifying supress amazon SlotId for maps details preload ad call LATAM_EXEMPT Privacy");
+		@Title("Verifying supress amazon SlotId for maps details preload ad call EXEMPT Privacy")
+		public void Verifying_supress_amazon_Slotid_mapsdetails_preload_adcall_EXEMPT_Privacy() throws Exception {
+			System.out.println("=================Verifying supress amazon SlotId for maps details preload ad call EXEMPT Privacy  testcase  Started =========================");
+			logStep("Verifying supress amazon SlotId for maps details preload ad call EXEMPT Privacy");
 			//Functions.get_aaxcal_map_details();
 			 Functions.verifyaax_SlotId_supress("2634dc9-b59f-4b2c-b281-bb3be291b7b6");
-			System.out.println("=================Verifying supress amazon SlotId for maps details preload ad call LATAM_EXEMPT Privacy testcase  End =========================");
+			System.out.println("=================Verifying supress amazon SlotId for maps details preload ad call EXEMPT Privacy testcase  End =========================");
 		}
 	
 
 		@Test(priority = 24, enabled = true)
-		@Title("Verifying supress amazon SlotId for daily details preload ad call LATAM_EXEMPT Privacy")
-		public void Verifying_supress_amazon_Slotid_Dailydetails_preload_adcall_Latam_EXEMPT_Privacy() throws Exception {
-			System.out.println("=================Verifying supress amazon SlotId for daily details preload ad call LATAM_EXEMPT Privacy testcase Started =========================");
-			logStep("Verifying supress amazon SlotId for daily details preload ad call LATAM_EXEMPT Privacy");
+		@Title("Verifying supress amazon SlotId for daily details preload ad call EXEMPT Privacy")
+		public void Verifying_supress_amazon_Slotid_Dailydetails_preload_adcall_EXEMPT_Privacy() throws Exception {
+			System.out.println("=================Verifying supress amazon SlotId for daily details preload ad call EXEMPT Privacy testcase Started =========================");
+			logStep("Verifying supress amazon SlotId for daily details preload ad call EXEMPT Privacy");
 			//Functions.get_aaxcal_Daily();
 			 Functions.verifyaax_SlotId_supress("6c5a145d-9198-48f4-adfd-08f05557eace");
 			System.out.println("=================Verifying supress amazon SlotId for daily details preload ad call LATAM_CO Privacy testcase End =========================");
 		}
 		@Test(priority = 26, enabled = true)
-		@Title("Verifying supress amazon SlotId for videos preload ad call for LATAM_EXEMPT Privacy")
-		public void Verifying_supress_amazon_Slotid_video_adcall_Latam_EXEMPT_Privacy() throws Exception {
-			System.out.println("=================Verifying supress amazon SlotId for videos preload ad call for LATAM_EXEMPT Privacy testcase Started =========================");
-			logStep("Verifying supress amazon SlotId for videos preload ad call for LATAM_EXEMPT Privacy");
+		@Title("Verifying supress amazon SlotId for videos preload ad call for EXEMPT Privacy")
+		public void Verifying_supress_amazon_Slotid_video_adcall_EXEMPT_Privacy() throws Exception {
+			System.out.println("=================Verifying supress amazon SlotId for videos preload ad call for EXEMPT Privacy testcase Started =========================");
+			logStep("Verifying supress amazon SlotId for videos preload ad call for EXEMPT Privacy");
 			 Functions.verifyaax_SlotId_supress("f71b7e17-6e34-4f6c-98f6-bbbe9f55586c");
-			System.out.println("=================Verifying supress amazon SlotId for videos preload ad call for LATAM_EXEMPT Privacy testcase  End =========================");
+			System.out.println("=================Verifying supress amazon SlotId for videos preload ad call for EXEMPT Privacy testcase  End =========================");
 		}	 
 
 
