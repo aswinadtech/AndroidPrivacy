c8ec50f9a8afc907c4de6b78fc9492695b965faf
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-02-16 15:39:19 +0530

    Update LGPD_Run.java

--- src/test/java/twc/Regression/TestCases/LGPD_Run.java
+++ src/test/java/twc/Regression/TestCases/LGPD_Run.java
@@ -60,7 +60,7 @@ public class LGPD_Run extends TwcAndroidBaseTest  {
 		// Enable rewriting on Charles install/launch TWC
 		this.proxy.enableRewriting();
 		this.proxy.startRecording();
-		
+		CharlesFunctions.archive_folder(“Charles”);
 		this.proxy.clearCharlesSession();
 		AppiumFunctions.LaunchAppWithFullReset();
 		System.out.println("Launching the app with full reset");
@@ -337,6 +337,7 @@ public class LGPD_Run extends TwcAndroidBaseTest  {
 	 System.out. println("=================Verifying npa=1 in detailed page ad call for LGPD privacy testcase started =========================" ); 
 	 Functions.validate_npa_video_ad_dontsellmyinformation();
 	  System.out. println("=================Verifying npa=1 in detailed page ad call for LGPD privacy testcase End =========================" );
+	  CharlesFunctions.archive_folder(“Charles”);
 	  }
 	
 	/*@BeforeTest
