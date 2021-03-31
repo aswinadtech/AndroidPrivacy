c4eb6edbf972fa6211aa538f2f33bd60d2707c67
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-03-31 21:19:28 +0530

    Update LGPD_Run.java

--- src/test/java/twc/Regression/TestCases/LGPD_Run.java
+++ src/test/java/twc/Regression/TestCases/LGPD_Run.java
@@ -338,9 +338,34 @@ public class LGPD_Run extends TwcAndroidBaseTest  {
 	 System.out. println("=================Verifying npa=1 in detailed page ad call for LGPD privacy testcase started =========================" ); 
 	 Functions.validate_npa_video_ad_dontsellmyinformation();
 	  System.out. println("=================Verifying npa=1 in detailed page ad call for LGPD privacy testcase End =========================" );
-	CharlesFunctions.archive_folder("Charles");
+	//CharlesFunctions.archive_folder("Charles");
 	 
 	  }
+	
+	
+		@Test(priority = 79, enabled = true)
+	@Title("Verify Criteo SDK inapp v2 call")
+	public void Verify_Criteo_SDK_inapp_v2_Call_privacy_optout_for_LGPD() throws Exception {
+		System.out.println("==============================================");
+		System.out.println("=========================== Criteo SDK inapp/v2 call ====================");
+		System.out.println("****** Criteo SDK inapp/v2 call validation Started");
+		logStep("****** Criteo SDK inapp/v2 call validation Started");
+		CharlesFunctions.createXMLFileForCharlesSessionFile();
+		Functions.verifyCriteo_inapp_v2_Call("Criteo", false);
+
+	}
+
+	@Test(priority = 80, enabled = true)
+	@Title("Verify Criteo SDK config app call")
+	public void Verify_Criteo_SDK_config_app_Call_privacy_optout_for_LGPD() throws Exception {
+		System.out.println("==============================================");
+		System.out.println("=========================== Criteo SDK config/app call ====================");
+		System.out.println("****** Criteo SDK config/app call validation Started");
+		logStep("****** Criteo SDK config/app call validation Started");
+		Functions.verifyCriteo_config_app_Call( "Criteo", false);
+		CharlesFunctions.archive_folder("charles");
+
+	}
 	
 	/*@BeforeTest
 	public  void lauchApp() throws Exception {
