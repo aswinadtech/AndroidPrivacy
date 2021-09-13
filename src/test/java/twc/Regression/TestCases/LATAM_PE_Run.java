8fd29c6b240a438349beee133477e85cf0cc5654
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-09-13 22:18:49 +0530

    Update LATAM_PE_Run.java

--- src/test/java/twc/Regression/TestCases/LATAM_PE_Run.java
+++ src/test/java/twc/Regression/TestCases/LATAM_PE_Run.java
@@ -270,9 +270,34 @@ public class LATAM_PE_Run extends TwcAndroidBaseTest {
 	public void Verifying_npa_equals_1_videos_adCall_Latam_PE_privacy()throws Exception {	  
 	 System.out. println("=================Verifying npa=1 in detailed page ad call for Latam_PE_privacy() privacy testcase started =========================" ); 
 	 Functions.validate_npa_video_ad_dontsellmyinformation();
-		CharlesFunctions.archive_folder("Charles");
+		//CharlesFunctions.archive_folder("Charles");
 	  System.out. println("=================Verifying npa=1 in detailed page ad call for Latam_PE_privacy() privacy testcase End =========================" );
 	  }
+	
+	
+	
+	@Test(priority = 740, enabled = true)
+		@Title("Verify Criteo SDK inapp v2 call")
+		public void Verify_Criteo_SDK_inapp_v2_Call_privacy_optout_for_Latam_PE() throws Exception {
+			System.out.println("==============================================");
+			System.out.println("=========================== Criteo SDK inapp/v2 call ====================");
+			System.out.println("****** Criteo SDK inapp/v2 call validation Started");
+			logStep("****** Criteo SDK inapp/v2 call validation Started");
+			CharlesFunctions.createXMLFileForCharlesSessionFile();
+			Functions.verifyCriteo_inapp_v2_Call("Criteo", false);
+
+		}
+
+		@Test(priority = 742, enabled = true)
+		@Title("Verify Criteo SDK config app call")
+		public void Verify_Criteo_SDK_config_app_Call_privacy_optout_for_Latam_PE() throws Exception {
+			System.out.println("==============================================");
+			System.out.println("=========================== Criteo SDK config/app call ====================");
+			System.out.println("****** Criteo SDK config/app call validation Started");
+			logStep("****** Criteo SDK config/app call validation Started");
+			CharlesFunctions.archive_folder("Charles");
+			Functions.verifyCriteo_config_app_Call("Criteo", false);			
+		}
 	
 	
 	
