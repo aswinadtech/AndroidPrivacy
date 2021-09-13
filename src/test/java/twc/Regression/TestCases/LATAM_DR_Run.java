ce159131630b78b1129399dc4e0c38517dbab2c8
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2021-09-13 11:01:42 +0530

    Update LATAM_DR_Run.java

--- src/test/java/twc/Regression/TestCases/LATAM_DR_Run.java
+++ src/test/java/twc/Regression/TestCases/LATAM_DR_Run.java
@@ -274,4 +274,29 @@ public class LATAM_DR_Run extends TwcAndroidBaseTest {
 	  CharlesFunctions.archive_folder("Charles");
 	}
 	
+		@Test(priority = 639, enabled = true)
+		@Title("Verify Criteo SDK inapp v2 call")
+		public void Verify_Criteo_SDK_inapp_v2_Call_privacy_optout_for_GDPR() throws Exception {
+			System.out.println("==============================================");
+			System.out.println("=========================== Criteo SDK inapp/v2 call ====================");
+			System.out.println("****** Criteo SDK inapp/v2 call validation Started");
+			logStep("****** Criteo SDK inapp/v2 call validation Started");
+			CharlesFunctions.createXMLFileForCharlesSessionFile();
+			Functions.verifyCriteo_inapp_v2_Call("Criteo", false);
+
+		}
+
+		@Test(priority = 640, enabled = true)
+		@Title("Verify Criteo SDK config app call")
+		public void Verify_Criteo_SDK_config_app_Call_privacy_optout_for_GDPR() throws Exception {
+			System.out.println("==============================================");
+			System.out.println("=========================== Criteo SDK config/app call ====================");
+			System.out.println("****** Criteo SDK config/app call validation Started");
+			logStep("****** Criteo SDK config/app call validation Started");
+			Functions.verifyCriteo_config_app_Call("Criteo", false);
+			CharlesFunctions.archive_folder("Charles");
+			
+
+		}
+	
 }
