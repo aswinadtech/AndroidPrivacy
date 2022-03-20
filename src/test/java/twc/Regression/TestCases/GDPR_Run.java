425e2d924bb3bfdc6f75ba0e41c2d3cdf908b45c
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2022-03-20 12:44:35 +0530

    Update GDPR_Run.java

--- src/test/java/twc/Regression/TestCases/GDPR_Run.java
+++ src/test/java/twc/Regression/TestCases/GDPR_Run.java
@@ -253,7 +253,7 @@ public class GDPR_Run extends TwcAndroidBaseTest  {
 			Functions.verifyaax_SlotId_supress("f71b7e17-6e34-4f6c-98f6-bbbe9f55586c");
 			//CharlesFunctions.archive_folder(“Charles”);
 			System.out.println("=================Verifying supress amazon SlotId for videos preload ad call for GDPR Privacy testcase  End =========================");
-				CharlesFunctions.archive_folder("Charles");
+				//CharlesFunctions.archive_folder("Charles");
 		    
 		}	 
 	
